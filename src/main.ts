ba3f84e071dc6839f173126cef1c08529c2918c0
Oscar Montes Camberos <o.montescamberos@gmail.com>
2025-10-25 16:54:53 -0600

    feat: enable CORS for the application
    - Configured CORS to allow requests from specified origins, enhancing security and accessibility for frontend applications.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ZodValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://app.frontend.com'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
