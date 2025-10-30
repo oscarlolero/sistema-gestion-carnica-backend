f29d753dc7bd8599fb8fde20ded5ff560c5545d8
Oscar Montes Camberos <o.montescamberos@gmail.com>
2025-10-30 17:16:11 -0600

    chore: simplify CORS configuration by allowing all origins

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ZodValidationPipe());
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:4173',
-      'https://app.frontend.com',
-    ],
+    origin: true,
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
