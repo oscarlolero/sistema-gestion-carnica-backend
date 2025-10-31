e0ff6898bd5e1278517cc3f45cbe0a4442335417
Oscar Montes Camberos <o.montescamberos@gmail.com>
2025-10-30 18:11:43 -0600

    feat: add HealthModule to application imports for improved health check functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ProductsModule, TicketsModule, CloudinaryModule],
+  imports: [ProductsModule, TicketsModule, CloudinaryModule, HealthModule],
   controllers: [],
   providers: [],
 })
